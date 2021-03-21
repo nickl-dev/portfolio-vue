4eb18cd40f46b5e4da43a3c55f85107fa2c4addd
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2021-03-21 11:03:10 -0700

    -created header -created mixins for header

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,41 @@
 <template>
-  <div>About</div>
+  <div class="about">
+    <header class="about__header">
+      <h1 class="about__name">Hi, I'm Nick</h1>
+      <vue-typer
+        class="about__occupation"
+        text="I'm a Web Developer 🚀"
+        :repeat="0"
+      ></vue-typer>
+    </header>
+  </div>
 </template>
 
 <script>
+import { VueTyper } from "vue-typer";
+
 export default {
   name: "About",
+
+  components: {
+    VueTyper,
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss">
+@import "@/styles/mixins.scss";
+
+.about {
+  &__header {
+    margin: 30vh 0;
+    text-align: center;
+  }
+  &__name {
+    @include name;
+  }
+  &__occupation {
+    @include occupation;
+  }
+}
+</style>
