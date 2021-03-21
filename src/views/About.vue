6c018c137ef477f14c2be2cf9ee7dda646aab4d2
NicholasSlade <60986212+NicholasSlade@users.noreply.github.com>
2021-03-21 12:04:00 -0700

    -created bio section with iamge and text -working on pdf issue

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,17 +8,41 @@
         :repeat="0"
       ></vue-typer>
     </header>
+    <section class="about__wrapper">
+      <div class="about__subwrapper">
+        <img :src="headshot" alt="Nick Lal" class="about__image" />
+        <div>
+          <p class="about__bio">
+            I love creating things that are visually and functionally smooth.
+            Front End Development is where I thrive - especially working with
+            Front End frameworks such as Vue and React. I'm passionate about UI,
+            debugging and finding new ways to solve new problems. I use my
+            skills to work with a great team on great projects while learning
+            something new every day.
+            <a :href="resume" class="about__resume" target="_blank">Resume</a>
+          </p>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import { VueTyper } from "vue-typer";
+import headshot from "@/assets/headshot.jpg";
 
 export default {
   name: "About",
 
   components: {
     VueTyper,
+  },
+
+  data: () => {
+    return {
+      headshot: headshot,
+      resume: require("../../public/nick-lal-webdev-resume.pdf"),
+    };
   },
 };
 </script>
@@ -36,6 +60,34 @@ export default {
   }
   &__occupation {
     @include occupation;
+  }
+  &__wrapper {
+    background: $stockBlack;
+    padding: 50px 5%;
+    @include tablet {
+      padding: 75px 10%;
+    }
+    @include desktop {
+      padding: 100px 13%;
+    }
+  }
+  &__subwrapper {
+    text-align: center;
+    @include tablet {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+  &__image {
+    width: 90%;
+    max-width: 300px;
+    margin: 0 auto;
+    border-radius: 5px;
+  }
+  &__bio {
+    margin: 30px 0;
+    @include bio;
   }
 }
 </style>
