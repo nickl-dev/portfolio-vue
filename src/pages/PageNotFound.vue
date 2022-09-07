1ae9c6ef8215db76914fd8cfc55d9a316d914d09
Nicholas Lal <nlalkta@gmail.com>
2022-09-07 14:48:20 -0600

    -added PageNotFound 404 page

---
+++ src/pages/PageNotFound.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>404</h1>
+    <p>Page not found</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFound'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/global.scss";
+
+div {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h1 {
+  font-size: 50px;
+}
+
+p {
+  margin-top: 20px;
+  font-size: $font18;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import About from '../pages/About.vue';
-import MyWork from '../pages/MyWork.vue';
-import Contact from '../pages/Contact.vue';
+import About from '@/pages/About';
+import MyWork from '@/pages/MyWork';
+import Contact from '@/pages/Contact';
+import PageNotFound from '@/pages/PageNotFound';
 
 Vue.use(VueRouter);
 
@@ -21,6 +22,11 @@ const routes = [
     path: '/contact',
     name: 'Contact',
     component: Contact
+  },
+  {
+    path: '*',
+    // name: 'PageNotFound',
+    component: PageNotFound
   }
 ];
 
