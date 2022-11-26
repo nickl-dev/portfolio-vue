1456da5cb19aab4c466f2e4d7b4087b035e0243f
Nicholas Lal <nlalkta@gmail.com>
2022-11-26 15:22:04 -0700

    -updated project demo links

--- src/pages/MyWork.vue
+++ src/pages/MyWork.vue
@@ -15,65 +15,71 @@
 </template>
 
 <script>
-import Project from "@/components/Project.vue"
-import GreetingsScreenshot from "@/assets/project-screenshots/greetings-screenshot.png"
-import CleanfolioScreenshot from "@/assets/project-screenshots/cleanfolio-screenshot.png"
-import MyGradientScreenshot from "@/assets/project-screenshots/mygradient-screenshot.png"
-import CanadaDrivesScreenshot from "@/assets/project-screenshots/canadadrives-homepage-2.0-screenshot.png"
+import Project from '@/components/Project.vue';
+import GreetingsScreenshot from '@/assets/project-screenshots/greetings-screenshot.png';
+import CleanfolioScreenshot from '@/assets/project-screenshots/cleanfolio-screenshot.png';
+import MyGradientScreenshot from '@/assets/project-screenshots/mygradient-screenshot.png';
+import CanadaDrivesScreenshot from '@/assets/project-screenshots/canadadrives-homepage-2.0-screenshot.png';
 
 export default {
-  name: "MyWork",
+  name: 'MyWork',
 
   components: { Project },
 
-  data () {
+  data() {
     return {
       projects: [
         {
-          title: "Canada Drives - Homepage 2.0",
-          description: "Canada Drives static website home page 2.0",
-          techStack: "Nuxt, JavaScript, Tailwind, SASS",
+          title: 'Canada Drives - Homepage 2.0',
+          description: 'Canada Drives static website home page 2.0',
+          techStack: 'JavaScript, Nuxt, Tailwind, SASS',
           image: CanadaDrivesScreenshot,
-          demo: "https://www.canadadrives.ca/"
+          demo: 'https://www.canadadrives.ca/'
         },
         {
-          title: "Cleanfolio",
-          description: "Clean and simple portfolio site template for developers",
-          techStack: "Vue, JavaScript, SASS",
+          title: 'Cleanfolio',
+          description:
+            'Clean and simple portfolio site template for developers',
+          techStack: 'JavaScript, Vue, SASS',
           image: CleanfolioScreenshot,
-          demo: "https://nlaldevcleanfolio.netlify.app/",
-          code: "https://github.com/nickl-dev/cleanfolio-vue"
+          demo: 'https://nickl-dev-cleanfolio.netlify.app/',
+          code: 'https://github.com/nickl-dev/cleanfolio-vue'
         },
         {
-          title: "Greetings",
-          description: "Social media application",
-          techStack: "React, JavaScript, Semantic UI, Express, Apollo, GraphQL, MongoDB",
+          title: 'Greetings',
+          description: 'Social media application',
+          techStack:
+            'JavaScript, React, Semantic UI, Express, Apollo, GraphQL, MongoDB',
           image: GreetingsScreenshot,
-          demo: "https://nlaldevgreetings.netlify.app/",
-          code: "https://github.com/nickl-dev/greetings"
+          demo: 'https://nickl-dev-greetings.netlify.app/',
+          code: 'https://github.com/nickl-dev/greetings'
         },
         {
-          title: "MyGradient",
-          description: "Dual color gradient generator",
-          techStack: "HTML, SASS, JavaScript",
+          title: 'MyGradient',
+          description: 'Dual color gradient generator',
+          techStack: 'HTML, SASS, JavaScript',
           image: MyGradientScreenshot,
-          demo: "https://nlaldevmygradient.netlify.app/",
-          code: "https://github.com/nickl-dev/mygradient"
+          demo: 'https://nickl-dev-mygradient.netlify.app/',
+          code: 'https://github.com/nickl-dev/mygradient'
         }
       ]
-    }
+    };
   }
-}
+};
 </script>
 
 <style lang="scss">
-@import "@/styles/global.scss";
+@import '@/styles/global.scss';
 
 .my-work {
   @include flexCenter;
   @include flexColumn;
   @include padding5;
-  &__heading { margin-top: 150px; }
-  @include desktop { margin-bottom: 30px; }
+  &__heading {
+    margin-top: 150px;
+  }
+  @include desktop {
+    margin-bottom: 30px;
+  }
 }
 </style>
